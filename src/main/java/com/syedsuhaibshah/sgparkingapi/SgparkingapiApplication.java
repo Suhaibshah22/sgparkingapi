8a599d981a56139f102d4a24a565c8545890adba
Suhaib Shah <suhaibshah22@gmail.com>
2023-10-01 20:42:19 +0800

    first commit

---
+++ src/main/java/com/syedsuhaibshah/sgparkingapi/SgparkingapiApplication.java
@@ -0,0 +1,13 @@
+package com.syedsuhaibshah.sgparkingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgparkingapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgparkingapiApplication.class, args);
+	}
+
+}
